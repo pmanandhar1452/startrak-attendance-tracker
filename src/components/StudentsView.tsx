8d7a6c8f5788607578fecebc46d46956913f7992
Prakash Manandhar <jsaaymi@gmail.com>
2025-09-25 12:18:27 -0400

    Updated StudentsView.tsx

--- src/components/StudentsView.tsx
+++ src/components/StudentsView.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { User, Mail, BookOpen, Search, Plus, Edit3, Trash2, Calendar, Phone, AlertCircle, Clock, GraduationCap } from 'lucide-react';
+import { User, Mail, BookOpen, Search, Plus, CreditCard as Edit3, Trash2, Calendar, Phone, AlertCircle, Clock, GraduationCap } from 'lucide-react';
 import { Student, WeeklySchedule, TimeSlot } from '../types';
 import { useStudents } from '../hooks/useStudents';
 
