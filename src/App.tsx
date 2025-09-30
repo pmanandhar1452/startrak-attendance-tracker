c8cb24d52772f6736829bad9bded43dcbc8b089c
Prakash Manandhar <jsaaymi@gmail.com>
2025-09-30 12:01:28 -0400

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -108,7 +108,6 @@ function App() {
             searchTerm={userManagementState.searchTerm}
             currentPage={userManagementState.currentPage}
             pageSize={userManagementState.pageSize}
-
         )
     }
   }
