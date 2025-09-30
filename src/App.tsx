3ab261a9693917741f78b6d277f8e4ece5c02f48
Prakash Manandhar <jsaaymi@gmail.com>
2025-09-30 12:38:34 -0400

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import { useEffect } from 'react';
-import { supabase } from './lib/supabase';
 import React, { useState, useEffect } from 'react';
 import { supabase } from './lib/supabase';
 import { AuthProvider } from './contexts/AuthContext';
